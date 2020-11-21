dbd2020e72fdf9e9d72d81e58b71e4ea16e4f4d5
guaifish <guaifish@hotmail.com>
2020-11-21 23:45:59 +0800

    配置axios发起登录请求

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+// 用户名：admin， 密码：1234456
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 
 Vue.config.productionTip = false
 
