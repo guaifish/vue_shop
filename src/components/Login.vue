9c09ae84c6b222ee8d82328c08ea37bb001a4636
guaifish <guaifish@hotmail.com>
2020-11-21 23:22:10 +0800

    实现登录前表单数据的预验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -16,7 +16,7 @@
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -49,6 +49,11 @@ export default {
   methods: {
     resetLoginForm () {
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate((valid) => {
+        console.log(valid)
+      })
     }
   }
 }
