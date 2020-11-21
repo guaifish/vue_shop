b62431180f46ad2b22bb09267314be6c4e257ea6
guaifish <guaifish@hotmail.com>
2020-11-21 21:36:30 +0800

    创建登录组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
