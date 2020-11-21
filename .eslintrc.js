6a35655b415d357f481481ea151a1ec81f0db861
guaifish <guaifish@hotmail.com>
2020-11-22 01:45:10 +0800

    home 页面的主体布局

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0,
-    'comma-dangle': ['error', 'always-multiline'],
+    'comma-dangle': ['error', 'only-multiline'],
     'quotes': ['error', 'double']
   }
 }
