3e2de9afbfcd80793f57d51c37ab3570a9330e54
guaifish <guaifish@hotmail.com>
2020-11-21 22:30:49 +0800

    绘制带图标的输入框”

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,11 +8,11 @@
       <el-form label-width="0px" class="login_form">
         <!-- 用户名 -->
         <el-form-item>
-          <el-input ></el-input>
+          <el-input prefix-icon="el-icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
         <el-form-item>
-          <el-input ></el-input>
+          <el-input prefix-icon="el-icon-lock"></el-input>
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
