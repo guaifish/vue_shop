86e84f7b0667915fc3d0b990670215a53af6189f
guaifish <guaifish@hotmail.com>
2020-11-22 00:22:46 +0800

    路由导航守卫控制页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -26,4 +26,15 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数， 表示放行
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 export default router
