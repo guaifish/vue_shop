b34ddfb014fcdb7b57dbd81520dc53667ec32feb
guaifish <guaifish@hotmail.com>
2020-11-21 22:21:45 +0800

    绘制登录表单区域

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,8 +2,24 @@
   <div class="login_container">
     <div class="login_box">
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt="">
+        <img src="../assets/logo.png" alt="" />
       </div>
+      <!-- 登录表单区域 -->
+      <el-form label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -48,5 +64,18 @@ export default {
     border-radius: 50%;
     background-color: #eee;
   }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
