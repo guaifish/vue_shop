9d4eb7836398814f1dc542da356a8207bca07b32
guaifish <guaifish@hotmail.com>
2020-11-22 23:22:41 +0800

    添加索引列

--- src/components/User/Users.vue
+++ src/components/User/Users.vue
@@ -24,6 +24,7 @@
 
         <!-- 用户列表区域 -->
         <el-table :data="userlist" border stripe>
+          <el-table-column label="#" type="index"></el-table-column>
           <el-table-column label="姓名" prop="username"></el-table-column>
           <el-table-column label="邮箱" prop="email"></el-table-column>
           <el-table-column label="电话" prop="mobile"></el-table-column>
