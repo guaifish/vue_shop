d0bd3c052eed9581c620ebb92aa8afd86675833e
guaifish <guaifish@hotmail.com>
2020-11-22 21:13:41 +0800

    通过axios拦截器添加token验证

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import axios from "axios"
 Vue.prototype.$http = axios
 // 用户名：admin， 密码：1234456
 axios.defaults.baseURL = "http://timemeetyou.com:8889/api/private/v1/"
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem("token")
+  console.log(config)
+  return config
+})
 
 Vue.config.productionTip = false
 
